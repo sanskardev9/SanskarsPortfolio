e92f6153fdd768271ed48004f870b1d3960e80fc
SanskarAvaronn <sanskar@avaronn.com>
2025-11-30 21:07:37 +0530

    update

--- src/components/parallaxBackground.jsx
+++ src/components/parallaxBackground.jsx
@@ -8,7 +8,7 @@ const ParallaxBackground = () => {
   const mountain2Y = useTransform(x, [0, 0.5], ["0%", "30%"]);
   const mountain1Y = useTransform(x, [0, 0.5], ["0%", "0%"]);
 
-  return (
+  return  ( 
     <section className="absolute inset-0 bg-black/40">
       <div className="relative h-screen overflow-y-hidden">
         {/* Background Sky */}
